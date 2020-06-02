acd081df6c72fbdab33716919f329a98c9353db9
KonTroAl <kostya98295@mail.ru>
2020-06-02 22:14:48 +0500

    Complit Half Task_1

---
+++ 1.py
@@ -0,0 +1,25 @@
+class Matrix:
+    def __init__(self, my_m):
+        self.str = [my_m[i][0] for i in range(len(my_m))]
+        self.col = [my_m[i][1] for i in range(len(my_m))]
+
+    def __add__(self, other):
+        A = []
+        B = []
+        for el in range(len(self.str)):
+            result_1 = self.str[el] + other.str[el]
+            A.append(str(result_1))
+        for arg in range(len((self.col))):
+            result_2 = self.col[arg] + other.col[arg]
+            B.append(str(result_2))
+
+        a = " ".join(A)
+        b = " ".join(B)
+        return f"{a}\n{b}"
+
+
+matrix_1 = Matrix([[1, 2], [3, 4]])
+matrix_2 = Matrix([[7, 8], [9, 10]])
+matrix_3 = Matrix([[5, 6], [11, 12]])
+
+print(matrix_1 + matrix_2 + matrix_3)
